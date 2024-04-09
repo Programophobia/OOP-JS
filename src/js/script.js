5368ad139dc998d4ba210880a68467ecf15c8df9
Anna Powaska <annapowaska@op.pl>
2024-04-09 10:56:50 +0200

    + - count in cart

--- src/js/script.js
+++ src/js/script.js
@@ -346,6 +346,47 @@ class Koszyk{
     const generatedHTML = templates.cartProduct(menuProduct);
     const generatedDOM = utils.createDOMFromHTML(generatedHTML);
     thisCart.dom.productList.appendChild(generatedDOM);
+    thisCart.products.push(new CartProduct(menuProduct, generatedDOM))
+    console.log(thisCart.products)
+  }
+}
+
+
+
+class CartProduct {
+  constructor(menuProduct, element){
+    const thisCartProduct = this;
+    element = menuProduct.element;
+
+    thisCartProduct.id = menuProduct.id;
+    thisCartProduct.name = menuProduct.name; 
+    thisCartProduct.amount = menuProduct.amount;
+    thisCartProduct.priceSingle = menuProduct.priceSingle;
+    thisCartProduct.price = menuProduct.price;
+    thisCartProduct.params = menuProduct.params;   
+
+    thisCartProduct.getElements(element);
+    thisCartProduct.initAmountWidget();
+    console.log('cart product:', thisCartProduct);
+  }
+
+  getElements(element){
+    const thisCartProduct = this;
+    thisCartProduct.dom = {};
+    thisCartProduct.dom.wrapper = element;
+    thisCartProduct.dom.amountWidget = document.querySelector(select.cartProduct.amountWidget);
+    thisCartProduct.dom.price = document.querySelector(select.cartProduct.price);
+    thisCartProduct.dom.edit = document.querySelector(select.cartProduct.edit);
+    thisCartProduct.dom.remove = document.querySelector(select.cartProduct.remove);
+  }
+
+  initAmountWidget(){
+    const thisCartProduct = this;
+    thisCartProduct.amountWidget = new AmountWidget(thisCartProduct.dom.amountWidget);
+    thisCartProduct.dom.amountWidget.addEventListener('redo', function() {
+      thisCartProduct.price = thisCartProduct.amountWidget.value * thisCartProduct.priceSingle;
+      thisCartProduct.dom.price.innerHTML = thisCartProduct.price;
+      });
   }
 }
 
